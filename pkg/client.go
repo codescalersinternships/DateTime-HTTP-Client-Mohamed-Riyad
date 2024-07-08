f4366bd96c8012bae0b210d9a5185618d6e03a18
Moahmed Riyad <mohamedelhenawy2012@yahoo.com>
2024-07-08 04:44:06 +0300

    fix: added comments to client.go

--- pkg/client.go
+++ pkg/client.go
@@ -6,33 +6,42 @@ import (
 	"time"
 )
 
+// Config holds the configuration parameters for the client.
 type Config struct {
-	Url  string
-	Port string
-}
-type Client struct {
-	config Config
-	client *http.Client
+	Url  string // URL where the API is hosted
+	Port string // Port number
 }
 
+// Client is an HTTP client that interacts with a remote API.
+type Client struct {
+	config Config       // Configuration for the client
+	client *http.Client // HTTP client with timeout settings
+}
+
+// NewConfig creates a new Config object with the provided URL and port.
 func NewConfig(url string, port string) *Config {
 	return &Config{url, port}
 }
+
+// NewClient creates a new Client object with the provided configuration.
 func NewClient(config Config) *Client {
 	return &Client{
 		config: config,
 		client: &http.Client{
-			Timeout: time.Second * 10,
+			Timeout: time.Second * 10, // Timeout set to 10 seconds
 		},
 	}
-
 }
+
+// GetDateTime performs an HTTP GET request to retrieve data from the specified endpoint.
 func (c *Client) GetDateTime(endPoint string) (*http.Response, error) {
+	// Retry logic with exponential backoff
 	return retry(10, 100*time.Millisecond, func() (*http.Response, error) {
 		return c.request(endPoint)
 	})
 }
 
+// request performs an HTTP GET request to the endpoint using client's configuration.
 func (c *Client) request(endPoint string) (*http.Response, error) {
 	url := c.config.Url + endPoint
 	req, err := http.NewRequest("GET", url, nil)
@@ -46,6 +55,8 @@ func (c *Client) request(endPoint string) (*http.Response, error) {
 
 	return resp, nil
 }
+
+// retry executes the provided function with retry logic and returns the response or error.
 func retry(attempts int, sleep time.Duration, fn func() (*http.Response, error)) (*http.Response, error) {
 	for i := 0; i < attempts; i++ {
 		resp, err := fn()
